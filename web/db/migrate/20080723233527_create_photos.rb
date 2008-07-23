d88c639024e932898e9d86758d78c895d39b393d
limi <lloydpick@gmail.com>
2008-07-23 23:53:34 +0000

    closes #3 - Created Photo's

---
+++ web/db/migrate/20080723233527_create_photos.rb
@@ -0,0 +1,19 @@
+class CreatePhotos < ActiveRecord::Migration
+  def self.up
+    create_table :photos do |t|
+      t.column "album_id", :integer
+      t.column "path", :string
+      t.column "title", :string
+      t.column "caption", :string
+      t.column "is_visible", :boolean, :default => true
+      t.column "created_at", :datetime
+      t.column "updated_at", :datetime
+      t.column "position", :integer
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :photos
+  end
+end
