694d92be64cda8cd4da65ee6f54f3399dd9fa3a7
limi <lloydpick@gmail.com>
2008-07-26 02:53:34 +0000

    closes #21 - Added ability to show individual photos

--- web/app/controllers/photos_controller.rb
+++ web/app/controllers/photos_controller.rb
@@ -1,2 +1,20 @@
 class PhotosController < ApplicationController
+  
+  def show
+    @album = Album.find_by_permalink(params[:permalink])
+    if not @album
+      raise "ZOMG NO ALBUM FOUND!"
+    end
+    @photo = Photo.find_by_permalink(params[:photolink])
+    if not @photo
+      raise "ZOMG NO PHOTO FOUND!"
+    else
+      if @photo.album.id == @album.id
+        @image = Image.find_by_id(@photo.image_id)
+      else
+        raise "PHOTO NOT PART OF ALBUM"
+      end
+    end
+  end
+  
 end
