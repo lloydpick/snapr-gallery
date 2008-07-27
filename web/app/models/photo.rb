89b030a16d40f1b8236a6e6e052a88a07dde6221
limi <lloydpick@gmail.com>
2008-07-27 01:33:38 +0000

    closes #33 - Created partial for updated photos

--- web/app/models/photo.rb
+++ web/app/models/photo.rb
@@ -3,6 +3,7 @@ class Photo < ActiveRecord::Base
   has_one :image, :conditions => 'parent_id is null'
   belongs_to :album
   
+  acts_as_ordered :order => 'position'
   has_permalink :title 
   
   def to_param
