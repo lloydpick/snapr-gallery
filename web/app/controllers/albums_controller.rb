911c629d40dab4282d2065fb99a5b30ffd27db60
limi <lloydpick@gmail.com>
2008-07-27 15:47:55 +0000

    closes #42 - Added positioning support to Albums

--- web/app/controllers/albums_controller.rb
+++ web/app/controllers/albums_controller.rb
@@ -5,8 +5,34 @@ class AlbumsController < ApplicationController
     if request.post?
       # Create a new user
       @album = Album.new(params[:album])
+      a = Album.find(:all)
       # Does it pass all validation?
       if @album.save
+        
+        if @album.position == ""
+          if a.size == 0
+            @album.position = 1
+            @album.save
+          else
+            @album.position = a.last.position+1
+            @album.save
+          end
+        else
+          if a.size == 0
+            @album.position = 1
+            @album.save
+          else
+            if @album.position > a.last.position+2
+              #if the photos position is greater than my last photo in my list
+              #change it to fit right after the last one, for example
+              #if my last item has a position of 6, let's set this new one to 
+              #a position of 7
+              @album.position = a.last.position+1
+              @album.save
+            end
+          end
+        end
+        
         # Save successful, redirect to album page
         @log = Log.new
         @log.user = current_user
@@ -19,6 +45,13 @@ class AlbumsController < ApplicationController
         # Show the error messages
         @album.valid?
       end
+    else
+      a = Album.find(:all)
+      if a.size == 0
+        @next = 1
+      else
+        @next = a.last.position+1
+      end
     end
   end
   
@@ -30,7 +63,7 @@ class AlbumsController < ApplicationController
   end
   
   def list
-    @albums = Album.find(:all, :conditions => { :is_visible => true }, :order => "position ASC, title ASC")
+    @albums = Album.find(:all, :conditions => { :is_visible => true, :parent_id => nil }, :order => "position ASC, title ASC")
   end
   
 end
