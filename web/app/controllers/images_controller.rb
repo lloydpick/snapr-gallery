4c636d8a6994257fc8f42aa6691fe32a3c00f1a3
limi <lloydpick@gmail.com>
2008-07-27 15:48:43 +0000

    closes #43 - Added positioning support to Photos

--- web/app/controllers/images_controller.rb
+++ web/app/controllers/images_controller.rb
@@ -2,6 +2,13 @@ class ImagesController < ApplicationController
       
   def new
     @image = Image.new
+    @album = Album.find_by_permalink(params[:permalink])
+    p = Photo.find(:all, :conditions => { :album_id => @album.id })
+    if p.size == 0
+      @next = 1
+    else
+      @next = p.last.position+1
+    end
   end
   
   def create
@@ -21,8 +28,34 @@ class ImagesController < ApplicationController
       @photo.caption = params[:photo][:caption]
       @photo.is_visible = params[:photo][:is_visible]
       @photo.position = params[:photo][:position]
+      p = Photo.find(:all, :conditions => { :album_id => @album.id })
       # Does it pass all validation?
       if @photo.save
+        if @photo.position == ""
+          if p.size == 0
+            @photo.position = 1
+            @photo.save
+          else
+            @photo.position = p.last.position+1
+            @photo.save
+          end
+        else
+          if p.size == 0
+            @photo.position = 1
+            @photo.save
+          else
+            if @photo.position > p.last.position+2
+              #if the photos position is greater than my last photo in my list
+              #change it to fit right after the last one, for example
+              #if my last item has a position of 6, let's set this new one to 
+              #a position of 7
+              @photo.position = p.last.position+1
+              @photo.save
+            end
+          end
+        end
+        @album.updated_at = Time.now
+        @album.save!
         @log = Log.new
         @log.user = current_user
         @log.item = "photo"
