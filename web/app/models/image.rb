33b65acc0cc98d28b5feffc8e473c17ea1e0b272
limi <lloydpick@gmail.com>
2008-07-27 02:08:06 +0000

    closes #37 - Sharpened Medium Images

--- web/app/models/image.rb
+++ web/app/models/image.rb
@@ -10,7 +10,7 @@ class Image < ActiveRecord::Base
                                   :square => 'c75x75',
                                   :thumbnail => 'c100x75',
                                   :small => 's240x180>',
-                                  :medium => '500x375>',
+                                  :medium => 's500x375>',
                                 }
 
   validates_as_attachment
@@ -25,6 +25,17 @@ class Image < ActiveRecord::Base
     if (size.is_a?(String) && size =~ /^crop: (\d*)x(\d*)/i) ||
         (size.is_a?(Array) && size.first.is_a?(String) &&
           size.first =~ /^crop: (\d*)x(\d*)/i)
+      bw = img.columns
+      bh = img.rows
+      base = bw > bh ? bw : bh;
+      if (base <= 800)
+        sigma = 1.9
+      elsif (base <= 1600)
+        sigma = 2.85
+      else
+        sigma = 3.8
+      end
+      img = img.sharpen(1, sigma)
       img.crop_resized!($1.to_i, $2.to_i)
       # We need to save the resized image in the same way the
       # orignal does.
