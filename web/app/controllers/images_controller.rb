f2d1e6c2849b4e9ecd862126c8be9e80e7cc6103
limi <lloydpick@gmail.com>
2008-07-28 22:24:20 +0000

    closes #61 - Allowed multi-file upload

--- web/app/controllers/images_controller.rb
+++ web/app/controllers/images_controller.rb
@@ -1,5 +1,7 @@
 class ImagesController < ApplicationController
-      
+
+  before_filter :login_required, :only => ["new", "create"]
+     
   def new
     @title = "Upload Image"
     @image = Image.new
@@ -10,6 +12,50 @@ class ImagesController < ApplicationController
     else
       @next = p.last.position+1
     end
+  end
+  
+  def swfupload
+    sleep(5)
+    # swfupload action set in routes.rb
+    @album = Album.find_by_permalink(params[:permalink])
+    @image = Image.new :uploaded_data => params[:Filedata]
+    if @image.save
+      @photo = Photo.new
+      @photo.album_id = @album.id
+      @photo.image_id = @image.id
+      @split = @image.filename.split('.')
+      @photo.title = @split[0]
+      @photo.is_visible = true
+      p = Photo.find(:all, :conditions => { :album_id => @album.id })
+      # Does it pass all validation?
+      if @photo.save
+        if p.size == 0
+          @photo.position = 1
+          @photo.save
+        else
+          if @photo.position > p.last.position+2
+            #if the photos position is greater than my last photo in my list
+            #change it to fit right after the last one, for example
+            #if my last item has a position of 6, let's set this new one to 
+            #a position of 7
+            @photo.position = p.last.position+1
+            @photo.save
+          end
+        end
+        @album.updated_at = Time.now
+        @album.save!
+        @log = Log.new
+        @log.user_id = params[:id]
+        @log.item = "photo"
+        @log.event = "add_photo"
+        @log.identifier = @photo.id
+        @log.save!
+      end
+      # This returns the thumbnail url for handlers.js to use to display the thumbnail
+      render :text => @image.public_filename(:square)
+    end
+  rescue
+    render :text => "Error"
   end
   
   def create
