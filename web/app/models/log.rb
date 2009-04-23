651503133ae6ac2565127ca576a0f9c2c5bd6828
Lloyd Pick <lloydpick@gmail.com>
2009-04-23 17:23:27 +0100

    Annotated Models/Fixtures

--- web/app/models/log.rb
+++ web/app/models/log.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+# Schema version: 20080729231414
+#
+# Table name: logs
+#
+#  id         :integer         not null, primary key
+#  user_id    :integer
+#  item       :string(255)
+#  event      :string(255)
+#  identifier :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Log < ActiveRecord::Base
   
   belongs_to :user
--- web/app/models/user.rb
+++ web/app/models/user.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+# Schema version: 20080729231414
+#
+# Table name: users
+#
+#  id           :integer         not null, primary key
+#  username     :string(255)
+#  emailaddress :string(255)
+#  password     :string(255)
+#  created_at   :datetime
+#  updated_at   :datetime
+#
+
 class User < ActiveRecord::Base
   
   has_many :logs
