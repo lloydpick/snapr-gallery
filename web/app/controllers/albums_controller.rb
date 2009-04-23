005b4e382a086db2beec7f7c24b243dc4cda5c4a
Lloyd Pick <lloydpick@gmail.com>
2009-04-23 17:30:28 +0100

    Album cleanup & upgrade

--- web/app/controllers/albums_controller.rb
+++ web/app/controllers/albums_controller.rb
@@ -1,102 +1,77 @@
 class AlbumsController < ApplicationController
   
-  before_filter :login_required, :only => ["new", "edit"]
+  before_filter :login_required, :only => ["new", "create", "edit", "update"]
+  before_filter :load_objects, :except => ["index", "new", "create"]
+
+  # Public Actions
   
-  def new
-    @title = "Create Album"
-    # Post Request
-    if request.post?
-      # Create a new user
-      @album = Album.new(params[:album])
-      a = Album.find(:all)
-      # Does it pass all validation?
-      if @album.save
-        
-        if @album.position == ""
-          if a.size == 0
-            @album.position = 1
-            @album.save
-          else
-            @album.position = a.last.position+1
-            @album.save
-          end
-        else
-          if a.size == 0
-            @album.position = 1
-            @album.save
-          else
-            if @album.position > a.last.position+2
-              #if the photos position is greater than my last photo in my list
-              #change it to fit right after the last one, for example
-              #if my last item has a position of 6, let's set this new one to 
-              #a position of 7
-              @album.position = a.last.position+1
-              @album.save
-            end
-          end
-        end
-        
-        # Save successful, redirect to album page
-        @log = Log.new
-        @log.user = current_user
-        @log.item = "album"
-        @log.event = "add_album"
-        @log.identifier = @album.id
-        @log.save!
-        redirect_to :action => "show", :permalink => @album.permalink
-      else
-        # Show the error messages
-        @album.valid?
-      end
-    else
-      a = Album.find(:all)
-      if a.size == 0
-        @next = 1
-      else
-        @next = a.last.position+1
-      end
-      @albums = Album.find(:all, :conditions => { :is_visible => true, :parent_id => nil }, :order => "position ASC")
+  def index
+    set_title("Album List")
+    @albums = Album.root.visible
+
+    respond_to do |format|
+      format.html
+      format.xml { render :xml => @albums }
+      format.json { render :json => @albums }
     end
   end
   
   def show
-    @album = Album.find_by_permalink(params[:permalink])
-    if not @album
-      raise "ZOMG NO ALBUM FOUND!"
+    set_title("Album - " + @album.title)
+
+    respond_to do |format|
+      format.html
+      format.xml { render :xml => @album }
+      format.json { render :json => @album }
     end
-    @title = "Album - " + @album.title
   end
-  
-  def list
-    @title = "Album List"
-    @albums = Album.find(:all, :conditions => { :is_visible => true, :parent_id => nil }, :order => "position ASC, title ASC")
-  end
-  
-  def list_text
-    @title = "Album Text List"
-    @albums = Album.find(:all, :conditions => { :is_visible => true, :parent_id => nil }, :order => "position ASC")
-  end
-  
+
+  # Private Actions
+
   def edit
-    @title = "Album Edit"
+    set_title("Edit Album - " + @album.title)
    
-    if request.post?
-      @album = Album.find_by_id(params[:album][:id])
-      @album.attributes = params[:album]
-  
-      if @album.save
-        @log = Log.new
-        @log.user = current_user
-        @log.item = "album"
-        @log.event = "edit_album"
-        @log.identifier = @album.id
-        @log.save!
-        redirect_to :action => "list"
-      end
-    else
-      @album = Album.find_by_permalink(params[:permalink])
+    respond_to do |format|
+      format.html
     end
-    
+  end
+
+  def update
+    @album.attributes = params[:album]
+
+    if @album.save
+      redirect_to albums_path
+    end
+  end
+
+  def new
+    set_title("Create Album")
+    @album = Album.new
+
+    respond_to do |format|
+      format.html
+    end
+  end
+
+  def create
+    @album = Album.new(params[:album])
+    @album.current_user = current_user
+
+    # Does it pass all validation?
+    if @album.save
+      redirect_to album_path(@album)
+    else
+      @album.valid?
+    end
+  end
+
+  private
+
+  def load_objects
+    @album = Album.find_by_permalink(params[:id])
+    if not @album
+      redirect_to albums_path
+    end
   end
   
 end
--- web/app/models/album.rb
+++ web/app/models/album.rb
@@ -1,12 +1,49 @@
+# == Schema Information
+# Schema version: 20090423150120
+#
+# Table name: albums
+#
+#  id          :integer         not null, primary key
+#  parent_id   :integer         default(0)
+#  title       :string(255)
+#  description :string(255)
+#  position    :integer
+#  is_visible  :boolean         default(TRUE)
+#  created_at  :datetime
+#  updated_at  :datetime
+#  permalink   :string(200)
+#
+
 class Album < ActiveRecord::Base
-  
+
+  attr_accessor :current_user
   has_many :photos, :order => :position
   has_permalink :title
   acts_as_tree :order => "title" 
   acts_as_list
-  
-  def self.find_roots
-    find(:all, :conditions => 'parent_id IS NULL')
+
+  default_scope :order => "title ASC"
+  named_scope :root, :conditions => { :parent_id => 0 }
+  named_scope :visible, :conditions => { :is_visible => true }
+
+  after_create :create_create_audit
+  after_update :create_edit_audit
+
+  def create_create_audit
+    create_audit("add_album")
+  end
+
+  def create_edit_audit
+    create_audit("edit_album")
+  end
+
+  def create_audit(event)
+    log = Log.new
+    log.user = current_user
+    log.item = "album"
+    log.event = event
+    log.identifier = self.id
+    log.save!
   end
 
   def to_param
