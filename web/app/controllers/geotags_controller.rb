edb8d75edb288b2f89090ee0fd563c4f77eed2b9
Lloyd Pick <lloydpick@gmail.com>
2009-04-24 20:07:49 +0100

    Geotags controller cleanup

--- web/app/controllers/geotags_controller.rb
+++ web/app/controllers/geotags_controller.rb
@@ -1,49 +1,59 @@
 class GeotagsController < ApplicationController
   
   before_filter :login_required, :only => ["new"]
+  before_filter :load_objects, :only => ["show"]
+
+  def index
+    set_title("Listing Geotag Locations")
+    @geotags = Geotag.find(:all, :order => "description DESC")
+
+    respond_to do |format|
+      format.html
+      format.xml { render :xml => @geotags }
+      format.json { render :json => @geotags }
+    end
+  end
+
+  def show
+    set_title("Showing Geotag Location - " + @geotag.description)
+    @map = GMap.new("map_div")
+    @map.control_init(:large_map => true,:map_type => true)
+    @map.set_map_type_init(GMapType::G_HYBRID_MAP);
+    @map.center_zoom_init([@geotag.latitude,@geotag.longitude],@geotag.zoom)
+    @map.overlay_init(GMarker.new([@geotag.latitude,@geotag.longitude], :title => "Geotag Location", :info_window => @geotag.description))
+
+    respond_to do |format|
+      format.html
+      format.xml { render :xml => @geotag }
+      format.json { render :json => @geotag }
+    end
+  end
   
   def new
-    @title = "Create Geotag Location"
-    # Post Request
-    if request.post?
-      # Create a new user
-      @geotag = Geotag.new(params[:geotag])
-      # Does it pass all validation?
-      if @geotag.save
-        
-        # Save successful, redirect to album page
-        @log = Log.new
-        @log.user = current_user
-        @log.item = "geotag"
-        @log.event = "add_geotag"
-        @log.identifier = @geotag.id
-        @log.save!
-        redirect_to :action => "list"
-      else
-        # Show the error messages
-        @geotag.valid?
-      end
+    set_title("Create Geotag Location")
+    @geotag = Geotag.new
+
+    respond_to do |format|
+      format.html
     end
   end
-  
-  def list
-    @title = "Listing Geotag Locations"
-    @geotags = Geotag.find(:all, :order => "description DESC")
-  end
-  
-  def show
-    @geotag = Geotag.find_by_id(params[:id])
-    if @geotag != nil
-      @title = "Showing Geotag Location - " + @geotag.description
-      @map = GMap.new("map_div")
-      @map.control_init(:large_map => true,:map_type => true)
-      @map.set_map_type_init(GMapType::G_HYBRID_MAP);
-      @map.center_zoom_init([@geotag.latitude,@geotag.longitude],@geotag.zoom)
-      @map.overlay_init(GMarker.new([@geotag.latitude,@geotag.longitude], :title => "Geotag Location", :info_window => @geotag.description))
+
+  def create
+    @geotag = Geotag.new(params[:geotag])
+
+    if @geotag.save
+      redirect_to geotag_path(@geotag)
     else
-      redirect_to :action => "list"
+      @geotag.valid?
     end
-    
+  end
+
+
+  private
+
+  def load_objects
+    @geotag = Geotag.find_by_id(params[:id])
+    redirect_to geotags_path unless @geotag
   end
   
 end
