b3085dbac5ca77b739ebf6f97696cadb9b4d7b18
Lloyd Pick <lloydpick@gmail.com>
2009-04-28 21:29:51 +0100

    Fixes production error

--- web/app/models/image.rb
+++ web/app/models/image.rb
@@ -126,15 +126,18 @@ class Image < ActiveRecord::Base
     long = EXIFR::JPEG.new(full_filename).gps_longitude
     long_ref = EXIFR::JPEG.new(full_filename).gps_longitude_ref
 
-    lat = lat[0].to_f + (lat[1].to_f / 60) + (lat[2].to_f / 3600)
-    long = long[0].to_f + (long[1].to_f / 60) + (long[2].to_f / 3600)
+    if lat && long
 
-    lat = lat * -1 if lat_ref == "S"      # (N is +, S is -)
-    long = long * -1 if long_ref == "W"   # (W is -, E is +)
+      lat = lat[0].to_f + (lat[1].to_f / 60) + (lat[2].to_f / 3600)
+      long = long[0].to_f + (long[1].to_f / 60) + (long[2].to_f / 3600)
 
-    gps = [lat, long]
-    return unless gps
-    gps
+      lat = lat * -1 if lat_ref == "S"      # (N is +, S is -)
+      long = long * -1 if long_ref == "W"   # (W is -, E is +)
+
+      gps = [lat, long]
+      gps
+
+    end
   end
    
 end
