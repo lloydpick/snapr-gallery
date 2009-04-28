101708550c4e2582636437cce19aa42548b8d3e1
Lloyd Pick <lloydpick@gmail.com>
2009-04-28 21:18:31 +0100

    Fixed "406 Not Acceptable" error

--- web/app/controllers/images_controller.rb
+++ web/app/controllers/images_controller.rb
@@ -32,41 +32,36 @@
   end
 
   def create
-    @image = Image.new(params[:image])
+    @image = Image.new :swf_uploaded_data => params[:Filedata]
+    @image.save!
 
-    respond_to do |format|
-      @image = Image.new :swf_uploaded_data => params[:Filedata]
-      @image.save!
+    @photo = Photo.new
+    @photo.album_id = @album.id
+    @photo.image_id = @image.id
+    @split = @image.filename.split('.')
+    @photo.title = @split[0]
+    @photo.is_visible = true
 
-      @photo = Photo.new
-      @photo.album_id = @album.id
-      @photo.image_id = @image.id
-      @split = @image.filename.split('.')
-      @photo.title = @split[0]
-      @photo.is_visible = true
-
-      p = Photo.find(:all, :conditions => { :album_id => @album.id })
-      if @photo.save
-        if p.size == 0
-          @photo.position = 1
+    p = Photo.find(:all, :conditions => { :album_id => @album.id })
+    if @photo.save
+      if p.size == 0
+        @photo.position = 1
+        @photo.save
+      else
+        if @photo.position > p.last.position+2
+          #if the photos position is greater than my last photo in my list
+          #change it to fit right after the last one, for example
+          #if my last item has a position of 6, let's set this new one to
+          #a position of 7
+          @photo.position = p.last.position+1
           @photo.save
-        else
-          if @photo.position > p.last.position+2
-            #if the photos position is greater than my last photo in my list
-            #change it to fit right after the last one, for example
-            #if my last item has a position of 6, let's set this new one to
-            #a position of 7
-            @photo.position = p.last.position+1
-            @photo.save
-          end
         end
-        @album.updated_at = Time.now
-        @album.save!
       end
-
-      format.html { render :text => @image.public_filename(:thumbnail) }
-      format.xml { render :nothing => true }
+      @album.updated_at = Time.now
+      @album.save!
     end
+
+    render :text => @image.public_filename(:thumbnail)
   end
   
 
