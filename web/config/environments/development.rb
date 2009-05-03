c56059d644255259b1899cd5924f8cd32405b3b6
Lloyd Pick <lloydpick@gmail.com>
2009-05-03 02:28:20 +0100

    Moved Google Analytics into the environment files [#3 state:resolved]

--- web/config/environments/development.rb
+++ web/config/environments/development.rb
@@ -15,3 +15,6 @@ config.action_controller.perform_caching             = false
 
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
+
+# Google Analytics ID, change from false to your ID to activate
+GOOGLE_ANALYTICS = false
--- web/config/environments/production.rb
+++ web/config/environments/production.rb
@@ -20,3 +20,6 @@ config.action_view.cache_template_loading            = true
 
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
+
+# Google Analytics ID, change from false to your ID to activate
+GOOGLE_ANALYTICS = "UA-2560469-4"
